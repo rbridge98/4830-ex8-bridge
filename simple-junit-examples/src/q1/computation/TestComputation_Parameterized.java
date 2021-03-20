894fe9d3eeca0aa0fe030f40179bded73bdb6ce9
rbridge98 <ryker.bridge@gmail.com>
2021-03-20 12:41:09 -0500

    junit testing complete

--- simple-junit-examples/src/q1/computation/TestComputation_Parameterized.java
+++ simple-junit-examples/src/q1/computation/TestComputation_Parameterized.java
@@ -18,16 +18,23 @@ public class TestComputation_Parameterized {
    public static Collection<Object[]> data() {
       return Arrays.asList(new Object[][] { //
     	  // TODO: Write Your Answer
+    	  {100, 1, 20},
+    	  {101, 2, 30},
+    	  {102, 3, 40}
             });
    }
 
    public TestComputation_Parameterized(int input1, int input2, int expected) {
  	  // TODO: Write Your Answer
+	   this.mInput1 = input1;
+	   this.mInput2 = input2;
+	   this.mExpected = expected;
    }
 
    @Test
    public void testGetDiscount() throws Exception {
  	  // TODO: Write Your Answer
-      // Assert.assertEquals(mExpected, TODO: Write Your Answer);
+	   int result = com.getDiscount(mInput1, mInput2);
+      Assert.assertEquals(mExpected, result);
    }
 }
